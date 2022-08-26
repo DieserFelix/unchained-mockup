fafb19b4dbd2d95e12f19fbc44cdd6bc4abaed76
Felix <felix2510@gmx.net>
2022-08-26 14:23:20 +0200

    Updated to use react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
-import ReactImageMagnify from "react-image-magnify";
-import "./App.css";
-import { Gallery } from "./components/gallery/Gallery";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ProductDetails } from "./ProductDetails";
 
 function App() {
   return (
-    <div style={{ width: "380px" }}>
-      <Gallery
-        sources={[
-          "/images/rg2_1.jpg",
-          "/images/rg2_2.jpg",
-          "/images/rg2_3.jpg",
-        ]}
-        generator={(source, i) => (
-          <ReactImageMagnify
-            key={i}
-            smallImage={{
-              alt: "",
-              width: 380,
-              height: 380,
-              isFluidWidth: false,
-              src: source,
-            }}
-            largeImage={{
-              src: source,
-              width: 850,
-              height: 850,
-            }}
-            enlargedImagePosition={"over"}
-          />
-        )}
-      />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/en/products/end-of-arm-effectors/grippers/finger-grippers/:productId"
+          element={<ProductDetails />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
